898f665452e64549c3345b66f62bb7ccd0d05af8
Evan Thomas <evan.thomas@epfl.ch>
2024-06-20 10:40:54 +0200

    Update env vars in startup script

--- app/start.sh
+++ app/start.sh
@@ -7,23 +7,23 @@
 # Add environment vars from env into R config. This is done
 # due to RShiny not passing system env vars to application
 cat <<EOF > secrets.R
-ENV <- '$METALP_ENV'
-MY_DB_NAME <- '$METALP_DB_NAME'
-MY_DB_HOST <- '$METALP_DB_HOSTNAME'
-MY_DB_PORT <- $METALP_DB_PORT
-MY_DB_USER <- '$METALP_DB_USERNAME'
-MY_DB_PWD <- '$METALP_DB_PASSWORD'
-NOREPLY_ADDRESS <- '$METALP_NOREPLY_ADDRESS'
-TO_ADDRESS <- '$METALP_TO_ADDRESS'
+ENV <- '$CNET_ENV'
+MY_DB_NAME <- '$CNET_DB_NAME'
+MY_DB_HOST <- '$CNET_DB_HOSTNAME'
+MY_DB_PORT <- $CNET_DB_PORT
+MY_DB_USER <- '$CNET_DB_USERNAME'
+MY_DB_PWD <- '$CNET_DB_PASSWORD'
+NOREPLY_ADDRESS <- '$CNET_NOREPLY_ADDRESS'
+TO_ADDRESS <- '$CNET_TO_ADDRESS'
 EOF
 
 # Write database dump config file
 cat <<EOF > ~/.my.cnf
 [client]
-user=$METALP_DB_USERNAME
-password=$METALP_DB_PASSWORD
-host=$METALP_DB_HOSTNAME
-port=$METALP_DB_PORT
+user=$CNET_DB_USERNAME
+password=$CNET_DB_PASSWORD
+host=$CNET_DB_HOSTNAME
+port=$CNET_DB_PORT
 EOF
 
 # Run RShiny
