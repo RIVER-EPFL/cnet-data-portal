12f60a741f1ca62a3a3c32882c089029c5098e7f
ndeluigi <32352578+ndeluigi@users.noreply.github.com>
2025-06-09 20:32:58 +0200

    Add files via upload

--- packages_installation.R
+++ packages_installation.R
@@ -4,5 +4,8 @@ library(renv)
 # Install dependencies from renv.lock file
 renv::restore(prompt=FALSE)
 
+# Install additional packages for discharge tool
+install.packages(c("pracma", "gridExtra", "signal"), repos="https://cran.r-project.org")
+
 # Requires a root user to install
 hrbrthemes::import_roboto_condensed()
