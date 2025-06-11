5d13659737caea532c86a0a13629ec102768397c
Evan Thomas <evan.thomas@epfl.ch>
2025-06-11 12:38:27 +0200

    Add pracma, gridExtra and signal into renv environment

--- packages_installation.R
+++ packages_installation.R
@@ -4,8 +4,5 @@ library(renv)
 # Install dependencies from renv.lock file
 renv::restore(prompt=FALSE)
 
-# Install additional packages for discharge tool
-install.packages(c("pracma", "gridExtra", "signal"), repos="https://cran.r-project.org")
-
 # Requires a root user to install
 hrbrthemes::import_roboto_condensed()
